7952cf53249ea55da906ced4038b6d43d37b62c6
varharrie <varharrie@gmail.com>
2016-11-26 12:12:55 +0800

    add types manager

---
+++ src/types.js
@@ -0,0 +1,17 @@
+const types = {}
+
+let i = 0
+
+export function create (description) {
+  const name = `[${i++}]${description}`
+  types[name] = true
+  return name
+}
+
+export function remove (name) {
+  delete types[name]
+}
+
+export function has (name) {
+  return !!types[name]
+}
